e10e1bcbf774ee55f3bafa1db420e67d4e9868bf
colo1211 <colo1211@naver.com>
2022-02-02 17:45:05 +0900

    folder structure setting

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+// component에 대한 export를 컨트롤 하는 파일
+// export { default as LandingPageHeader } from './LandingPage/Header';
---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Route를 작성하는 공간, App.tsx
+    </div>
+  );
+}
+
+export default App;
