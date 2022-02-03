80cbc68ae907233e6cc82b44c83e44d5867d5819
colo1211 <colo1211@naver.com>
2022-02-04 03:58:04 +0900

    feat: Login-2 Page UI 개발 완료 및 animation 기능 추가(#1)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
+import IntroPage from './IntroPage';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
+        <Route path='/intro' element={<IntroPage />} />
       </Routes>
     </Router>
   );
