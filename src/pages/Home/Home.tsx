bbbe0a887e8c8aec549336724835774b8cf61aad
wish1030 <wish@khu.ac.kr>
2022-02-06 23:14:26 +0900

    feat: Navigation Bar component(#6)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.scss';
 import Header from '../../components/common/Header';
+import NavigationBar from '../../components/common/NavigationBar';
 
 function Home() {
   return (
     <div className="home-wrapper">
       <Header />
+      <main>hello</main>
+      <NavigationBar />
     </div>
   );
 }
