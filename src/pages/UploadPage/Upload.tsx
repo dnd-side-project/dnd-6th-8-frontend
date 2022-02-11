8548004f37a5b44ff90369ee632f47c3de4c0a93
wish1030 <wish@khu.ac.kr>
2022-02-11 21:17:05 +0900

    feat: 필수작성 공지 component(#9)

--- src/pages/UploadPage/Upload.tsx
+++ src/pages/UploadPage/Upload.tsx
@@ -4,8 +4,14 @@ import Header from '../../components/UploadPage/Header';
 
 function Upload() {
   return (
-    <div>
+    <div className="upload-wrapper">
       <Header isLeftButtonX={false} isRightButtonSave title="표지작성" />
+      <main>
+        <div className="notice">
+          <img src="imgs/UploadPage/emoji_light_archiving.png" alt="light" />
+          아래의 정보는 <span>필수</span>로 작성해주세요.
+        </div>
+      </main>
     </div>
   );
 }
