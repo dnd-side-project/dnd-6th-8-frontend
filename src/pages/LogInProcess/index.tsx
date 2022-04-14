909a71c2293c2a4d8e3e2b9c061737352e19f9e4
colo1211 <colo1211@naver.com>
2022-04-14 19:30:12 +0900

    fix: introPage(#56)

--- src/pages/LogInProcess/index.tsx
+++ src/pages/LogInProcess/index.tsx
@@ -24,7 +24,7 @@ function LogInProcess() {
       if (signupCheck) {
         navigate('/home');
       } else {
-        navigate('/surveyStart');
+        navigate('/intro');
       }
     }
   }, []);
