8b9d97a0505fbf9b71a509ed642c9c2e4aa5d24d
kyungwonkim <colo1211@naver.com>
2022-08-04 20:04:44 +0900

    feat: 문자에 맞는 이모지 매칭(#72)

--- src/components/WallPaper/WallPaperDetailView/TabBody/index.tsx
+++ src/components/WallPaper/WallPaperDetailView/TabBody/index.tsx
@@ -1,35 +1,56 @@
 /* eslint-disable react/jsx-key */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Pagination } from 'swiper';
 import LocPin from '../../../../assets/icons/WallPaper/dayfeed/ic_location_purple 1.png';
 import './style.scss';
 import { RootState } from '../../../../modules';
+import Sun from '../../../../assets/icons/Weather/맑음.png';
+import Cloudy from '../../../../assets/icons/Weather/흐림.png';
+import Rainy from '../../../../assets/icons/Weather/비.png';
+import Snow from '../../../../assets/icons/Weather/눈.png';
 
 type TabBodyProps = {
   index: number;
 };
 
 function TabBody({ index }: TabBodyProps) {
-  const [imgClick, setImgClick] = useState(true);
-  const {data} = useSelector(
-    (state: RootState) => state.dayFeed,
-  );
+  const { data } = useSelector((state: RootState) => state.dayFeed);
+  const [weather, setWeather] = useState('');
+  useEffect(() => {
+    switch (data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.weather) {
+      case '맑음':
+        setWeather(Sun);
+        break;
+      case '흐림':
+        setWeather(Cloudy);
+        break;
+      case '비':
+        setWeather(Rainy);
+        break;
+      case '눈':
+        setWeather(Snow);
+        break;
+      default:
+        break;
+    }
+  }, [data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.weather]);
   SwiperCore.use([Pagination]);
   return (
     <div className="tabbody-wrapper">
       <div className="date-weather">
         <p className="body-date">{data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.date}</p>
-        <div>{data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.weather}</div>
+        <div>
+          <img src={weather} alt="날씨" />
+          <p>{data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.weather}</p>
+        </div>
       </div>
 
       <div className="carousel-wrapper">
-        {data.daysObjAndSubResponseDto[index].imgUrl.map((url : string) => {
-            return (
-                <img className="day-img" src={url} alt="이미지캐루셀" width="150px" height="150px" />
-            );
-          })}
+        {data.daysObjAndSubResponseDto[index].imgUrl.map((url: string) => {
+          return <img className="day-img" src={url} alt="이미지캐루셀" width="150px" height="150px" />;
+        })}
       </div>
       <div className="content-wrapper">
         {/* <Swiper
@@ -69,20 +90,26 @@ function TabBody({ index }: TabBodyProps) {
             <div className="today-line" />
             하루의 여정
           </p>
-          <div className="archiving-wrapper">{data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.travelDescription}</div>
+          <div className="archiving-wrapper">
+            {data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.travelDescription}
+          </div>
         </div>
         <div className="today-wrapper">
           <p>
             <div className="today-line" /> 하루의 감정
           </p>
-          <div className="archiving-wrapper">{data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.emotionDescription}</div>
+          <div className="archiving-wrapper">
+            {data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.emotionDescription}
+          </div>
         </div>
         <div className="today-wrapper">
           <p>
             <div className="today-line" />
             여행 꿀팁
           </p>
-          <div className="archiving-wrapper">{data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.tipDescription}</div>
+          <div className="archiving-wrapper">
+            {data.daysObjAndSubResponseDto[index].daysSubjectiveResponseDto.tipDescription}
+          </div>
         </div>
       </div>
     </div>
